2f57f9f673b3f23540934614cbbe49103d624671
dbuscombe-usgs <dbuscombe@gmail.com>
2021-11-04 21:13:58 -0700

    11/4/21

--- utils/gen_images_and_labels_4_zoo.py
+++ utils/gen_images_and_labels_4_zoo.py
@@ -131,8 +131,8 @@ def make_jpegs():
             im = np.squeeze(data['orig_image'].astype('uint8'))[:,:,:3]
         else:
             if data['image'].shape[-1]==4:
-                im=np.squeeze(data['image'].astype('uint8'))[:,:,1:]
-                band4=np.squeeze(data['image'].astype('uint8'))[:,:,0]
+                im=np.squeeze(data['image'].astype('uint8'))[:,:,:-1]
+                band4=np.squeeze(data['image'].astype('uint8'))[:,:,-1]
             else:
                 im = np.squeeze(data['image'].astype('uint8'))[:,:,:3]
 
